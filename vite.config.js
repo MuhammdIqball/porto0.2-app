c57a5766dae6886e808799a0c07a06d7cbe56911
muhammdiqball <iqbalqoriemail@gmail.com>
2024-06-27 00:35:41 +0700

    add deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/porto0.2-appporto0.2-app/",
   css: {
     postcss: {
       plugins: [tailwindcss()],
